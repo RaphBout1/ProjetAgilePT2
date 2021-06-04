3c334fe644e456cc95488bd5edfb2ce016b0ee61
gbertet <gauthier.bertet@etu.u-bordeaux.fr>
2021-06-04 16:59:32 +0200

    Assemblage de toutes les classes admin + correction US3,4 et 9

--- LocationMusique/PT2/Admin.cs
+++ LocationMusique/PT2/Admin.cs
@@ -14,6 +14,8 @@ namespace PT2
     {
         MusiquePT2_DEntities musiqueSQL = new MusiquePT2_DEntities();
         private ABONNÉS utilisateurCourant;
+        private HashSet<ABONNÉS> abonnésPurgeables = new HashSet<ABONNÉS>();
+        HashSet<ALBUMS> lesPlusEmprunté = new HashSet<ALBUMS>();
         public Admin()
         {
             InitializeComponent();
@@ -48,12 +50,75 @@ namespace PT2
 
                 foreach (EMPRUNTER m in lesLivresEmpruntes)
                 {
-
-                    if (m.dejaRenouvelé)
+                    if (m.DATE_EMPRUNT.AddDays(m.ALBUMS.GENRES.DÉLAI).CompareTo(m.DATE_RETOUR_ATTENDUE) < 0 && m.DATE_RETOUR == null) //à vérifier
                     {
                         listBox1.Items.Add(m);
                     }
 
+                }
+            }
+        }
+
+        public void DixPlusVue()
+        {
+            lesPlusEmprunté.Clear();
+            var lesAlbums = from a in musiqueSQL.ALBUMS select a;
+            foreach (ALBUMS a in lesAlbums)
+            {
+                if (lesPlusEmprunté.Count() <= 10)
+                {
+                    lesPlusEmprunté.Add(a);
+                }
+                else
+                {
+                    foreach (ALBUMS pe in lesPlusEmprunté)
+                    {
+                        if (a.EMPRUNTER.Count > pe.EMPRUNTER.Count)
+                        {
+                            lesPlusEmprunté.Remove(pe);
+                            lesPlusEmprunté.Add(a);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void abonnésAPurger()
+        {
+            var abonnés = from e in musiqueSQL.EMPRUNTER
+                          join alb in musiqueSQL.ALBUMS on e.CODE_ALBUM equals alb.CODE_ALBUM
+                          join abo in musiqueSQL.ABONNÉS on e.CODE_ABONNÉ equals abo.CODE_ABONNÉ
+                          where e.DATE_EMPRUNT.CompareTo(DateTime.UtcNow.AddYears(-1)) <= 0
+                          select abo;
+            foreach (ABONNÉS a in abonnés)
+            {
+                if (!abonnésPurgeables.Contains(a))
+                    abonnésPurgeables.Add(a);
+            }
+        }
+
+        private void listeAbonnésPurgeables()
+        {
+            abonnésAPurger();
+            if (abonnésPurgeables != null)
+            {
+                string s = "";
+                for (int i = 0; i < abonnésPurgeables.Count; i++)
+                {
+                    s = s + abonnésPurgeables.ElementAt(i) + "   ";
+                }
+                MessageBox.Show(s);
+            }
+        }
+
+        private void purgerAbonné(int codeAbonné)
+        {
+            abonnésAPurger();
+            foreach (ABONNÉS a in abonnésPurgeables)
+            {
+                if (a.CODE_ABONNÉ == codeAbonné)
+                {
+                    musiqueSQL.ABONNÉS.Remove(a);
                 }
             }
         }
