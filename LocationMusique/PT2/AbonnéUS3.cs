3a122ee23d8563dd5e9f1d497aa79bfd83562194
gbertet <gauthier.bertet@etu.u-bordeaux.fr>
2021-06-04 11:31:36 +0200

    US3

---
+++ LocationMusique/PT2/AbonnéUS3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PT2
+{
+    public partial class Utilisateur : Form
+    {
+        private ABONNÉS utilisateur;
+        public void ProlongerEmprunt(int codeAlbum)
+        {
+            foreach (EMPRUNTER e in utilisateur.EMPRUNTER)
+            {
+                ALBUMS a = e.ALBUMS;
+                if (a.CODE_ALBUM == codeAlbum)
+                {
+                    if (!e.dejaRenouvelé && e.DATE_RETOUR == null)
+                    {
+                        e.dejaRenouvelé = true;
+                        e.DATE_RETOUR_ATTENDUE = e.DATE_RETOUR_ATTENDUE.AddMonths(1);
+                    }
+                }
+            }
+            
+        }
+
+        public void ProlongerTousEmprunts()
+        {
+
+        }
+    }
+}
---
+++ LocationMusique/PT2/EmprunterUS3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PT2
+{
+    public partial class EMPRUNTER
+    {
+        public bool dejaRenouvelé = false;
+    }
+}
