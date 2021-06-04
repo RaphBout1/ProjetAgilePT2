8947e6c7815bf9e560ecd9c6f355aaf3565e3580
theo theo <theo.desc@hotmail.com>
2021-06-04 17:03:22 +0200

    inscription form in progress

---
+++ LocationMusique/PT2/Inscription.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace PT2
+{
+    partial class Inscription
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginText = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.Label();
+            this.passwordText = new System.Windows.Forms.TextBox();
+            this.nomButton = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // loginText
+            // 
+            this.loginText.Location = new System.Drawing.Point(396, 87);
+            this.loginText.Name = "loginText";
+            this.loginText.Size = new System.Drawing.Size(100, 20);
+            this.loginText.TabIndex = 0;
+            // 
+            // login
+            // 
+            this.login.AutoSize = true;
+            this.login.Location = new System.Drawing.Point(140, 87);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(29, 13);
+            this.login.TabIndex = 1;
+            this.login.Text = "login";
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(143, 147);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(52, 13);
+            this.password.TabIndex = 2;
+            this.password.Text = "password";
+            // 
+            // passwordText
+            // 
+            this.passwordText.Location = new System.Drawing.Point(396, 139);
+            this.passwordText.Name = "passwordText";
+            this.passwordText.Size = new System.Drawing.Size(100, 20);
+            this.passwordText.TabIndex = 3;
+            // 
+            // nomButton
+            // 
+            this.nomButton.Location = new System.Drawing.Point(396, 192);
+            this.nomButton.Name = "nomButton";
+            this.nomButton.Size = new System.Drawing.Size(100, 20);
+            this.nomButton.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(396, 247);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(143, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(143, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(143, 306);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(396, 306);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // Inscription
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.nomButton);
+            this.Controls.Add(this.passwordText);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.loginText);
+            this.Name = "Inscription";
+            this.Text = "Inscription";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox loginText;
+        private System.Windows.Forms.Label login;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.TextBox passwordText;
+        private System.Windows.Forms.TextBox nomButton;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
