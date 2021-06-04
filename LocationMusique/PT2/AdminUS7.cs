77133740b6c42d34ea072fad1f8c2e68d3d377eb
Paolini Timothe <timothe.paolini@etu.u-bordeaux.fr>
2021-06-04 15:15:43 +0200

    rajout d'un Hashset contenant les 10 plus vues et sa fonction d'actualisation

---
+++ LocationMusique/PT2/AdminUS7.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PT2
+{
+    public partial class Admin : Form
+    {
+        HashSet<ALBUMS> lesPlusEmprunté=new HashSet<ALBUMS>();
+
+        public void DixPlusVue()
+        {
+            lesPlusEmprunté.Clear();
+            var lesAlbums = from a in musiqueSQL.ALBUMS select a;
+            foreach(ALBUMS a in lesAlbums)
+            {
+                if (lesPlusEmprunté.Count()<=10)
+                {
+                    lesPlusEmprunté.Add(a);
+                }
+                else
+                {
+                    foreach(ALBUMS pe in lesPlusEmprunté)
+                    {
+                        if(a.EMPRUNTER.Count > pe.EMPRUNTER.Count)
+                        {
+                            lesPlusEmprunté.Remove(pe);
+                            lesPlusEmprunté.Add(a);
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
