5132514aff276362427d62a17061df503f423570
gbertet <gauthier.bertet@etu.u-bordeaux.fr>
2021-06-04 16:26:01 +0200

    Regroupement de la classe Utilisateur

--- LocationMusique/PT2/Utilisateur.cs
+++ LocationMusique/PT2/Utilisateur.cs
@@ -13,10 +13,94 @@ namespace PT2
     public partial class Utilisateur : Form
     {
 
+        private ABONNÉS utilisateur;
+        Dictionary<GENRES, int> listeGenreEmprunte = new Dictionary<GENRES, int>();
+        List<ALBUMS> listeAlbumsRecommande = new List<ALBUMS>();
+
         public Utilisateur()
         {
             InitializeComponent();
             ActualiseListeEmprunté();
         }
+
+        private void ActualiseListeEmprunté()
+        {
+            if (utilisateur != null)
+            {
+                foreach (EMPRUNTER e in utilisateur.EMPRUNTER)
+                {
+                    ALBUMS a = e.ALBUMS;
+                    listBoxConsultEmprunt.Items.Add(a.TITRE_ALBUM + "   " + a.GENRES.LIBELLÉ_GENRE);
+                }
+            }
+        }
+
+        public void ProlongerEmprunt(int codeAlbum)
+        {
+            foreach (EMPRUNTER e in utilisateur.EMPRUNTER)
+            {
+                ALBUMS a = e.ALBUMS;
+                if (a.CODE_ALBUM == codeAlbum || codeAlbum == 9999)
+                {
+                    if (!e.dejaRenouvelé && e.DATE_RETOUR == null)
+                    {
+                        e.dejaRenouvelé = true;
+                        e.DATE_RETOUR_ATTENDUE = e.DATE_RETOUR_ATTENDUE.AddMonths(1);
+                        MessageBox.Show("L'emprunt de l'album " + a.TITRE_ALBUM + " a bien été renouvelé.");
+                    }
+                }
+                if (e.DATE_EMPRUNT.AddMonths(1).AddDays(a.GENRES.DÉLAI).CompareTo(e.DATE_RETOUR_ATTENDUE.AddMonths(1)) == -1)
+                {
+
+                }
+            }
+
+        }
+
+        public void ProlongerTousEmprunts()
+        {
+            ProlongerEmprunt(9999);
+        }
+
+        #region Recommandation
+        private void Recommandation()
+        {
+            listeGenreEmprunte.Clear();
+            ListerGenreEmprunte();
+            ListageRecommandeAlbum();
+        }
+        #region Calcule Recommandation
+        private void ListerGenreEmprunte()
+        {
+            foreach (EMPRUNTER e in utilisateur.EMPRUNTER)
+            {
+                if (listeGenreEmprunte.ContainsKey(e.ALBUMS.GENRES))
+                {
+                    listeGenreEmprunte[e.ALBUMS.GENRES] += 1;
+                }
+                else
+                {
+                    listeGenreEmprunte.Add(e.ALBUMS.GENRES, 1);
+                }
+            }
+        }
+
+        private void ListageRecommandeAlbum()
+        {
+            Dictionary<int, ALBUMS> listeAlbum = new Dictionary<int, ALBUMS>();
+            foreach (KeyValuePair<GENRES, int> gi in listeGenreEmprunte)
+            {
+                foreach (ALBUMS a in gi.Key.ALBUMS)
+                {
+                    listeAlbum.Add(a.EMPRUNTER.Count + (1000 * gi.Value), a);
+                }
+            }
+            foreach (KeyValuePair<int, ALBUMS> ia in listeAlbum.OrderBy(importance => importance.Key))
+            {
+                listeAlbumsRecommande.Add(ia.Value);
+            }
+        }
+        #endregion
+        #endregion
     }
 }
