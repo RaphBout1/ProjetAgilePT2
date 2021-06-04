5b3f9fca34b3643fb5e47d944aa254bda0a3a344
KrakenKillian <killian.mannarelli@gmail.com>
2021-06-04 15:38:53 +0200

    Fix + ajout toString

--- LocationMusique/PT2/Admin.Designer.cs
+++ LocationMusique/PT2/Admin.Designer.cs
@@ -29,13 +29,23 @@ namespace PT2
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(103, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
             // 
             // Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
             this.Name = "Admin";
             this.Text = "Admin";
             this.Load += new System.EventHandler(this.Admin_Load);
@@ -44,5 +54,7 @@ namespace PT2
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
