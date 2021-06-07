c378a94434c59f196d2ebc0eeaa1c5b9c6de2f35
Aurelien <aluxey@IUT.BX1>
2021-06-07 09:59:36 +0200

    Ajout de US8

---
+++ LocationMusique/PT2/AdminUS8.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+
+namespace PT2
+{
+    
+    public partial class Admin
+    {
+        HashSet<ALBUMS> albumPasEmprunter1 = new HashSet<ALBUMS>();
+        private  void albumPasEmprunter()
+        {
+            var emprunt = from e in musiqueSQL.EMPRUNTER select e;
+            foreach(EMPRUNTER e in emprunt)
+            {
+                if(e.DATE_EMPRUNT.CompareTo(DateTime.UtcNow.AddYears(-1)) <= 0)
+                {
+                    if (!albumPasEmprunter1.Contains(e.ALBUMS))
+                    {
+                        albumPasEmprunter1.Add(e.ALBUMS);
+                    }
+                }
+            }
+        }
+
+        private  void listeAlbumPasEmprunte()
+        {
+            albumPasEmprunter();
+            if (albumPasEmprunter1 != null)
+            {
+                string s = "";
+                for (int i = 0; i < albumPasEmprunter1.Count; i++)
+                {
+                    s = s + albumPasEmprunter1.ElementAt(i).ToString() + "   ";
+                }
+                MessageBox.Show(s);
+            }
+        }
+    }
+}
