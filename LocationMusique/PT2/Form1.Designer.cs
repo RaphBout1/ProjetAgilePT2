50f757ddbb69e48d47ee9d4c8f8a947a4647802f
KrakenKillian <killian.mannarelli@gmail.com>
2021-06-07 08:37:22 +0200

    Début Form1

--- LocationMusique/PT2/Form1.Designer.cs
+++ LocationMusique/PT2/Form1.Designer.cs
@@ -29,13 +29,69 @@ namespace PT2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pseudo = new System.Windows.Forms.TextBox();
+            this.mdp = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connexion";
+            // 
+            // pseudo
+            // 
+            this.pseudo.Location = new System.Drawing.Point(31, 88);
+            this.pseudo.Name = "pseudo";
+            this.pseudo.Size = new System.Drawing.Size(100, 20);
+            this.pseudo.TabIndex = 1;
+            // 
+            // mdp
+            // 
+            this.mdp.Location = new System.Drawing.Point(31, 136);
+            this.mdp.Name = "mdp";
+            this.mdp.PasswordChar = '*';
+            this.mdp.Size = new System.Drawing.Size(100, 20);
+            this.mdp.TabIndex = 2;
+            this.mdp.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mdp);
+            this.Controls.Add(this.pseudo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pseudo;
+        private System.Windows.Forms.TextBox mdp;
+        private System.Windows.Forms.Button button1;
     }
 }
 
