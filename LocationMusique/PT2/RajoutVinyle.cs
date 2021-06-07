48c7db50792a5252249c5917b5c1c16c39c898b1
Paolini Timothe <timothe.paolini@etu.u-bordeaux.fr>
2021-06-07 10:36:13 +0200

    Continuation du code, Petit problème avec SaveChanges qui sera réglé plus tard car non prioritaire

--- LocationMusique/PT2/RajoutVinyle.cs
+++ LocationMusique/PT2/RajoutVinyle.cs
@@ -13,9 +13,29 @@ namespace PT2
 {
     public partial class RajoutVinyle : Form
     {
+        MusiquePT2_DEntities musiqueSQL = new MusiquePT2_DEntities();
         public RajoutVinyle()
         {
             InitializeComponent();
+            initGenreEditeur();
+        }
+
+        private void initGenreEditeur()
+        {
+            var editeur = from e in musiqueSQL.EDITEURS
+                          orderby e.NOM_EDITEUR
+                          select e;
+            foreach(EDITEURS e in editeur)
+            {
+                comboBoxEditeur.Items.Add(e);
+            }
+            var genre = from g in musiqueSQL.GENRES
+                        orderby g.LIBELLÉ_GENRE
+                        select g;
+            foreach (GENRES g in genre)
+            {
+                comboBoxGenre.Items.Add(g);
+            }
         }
 
         private bool VerifPresenceNecessaire()
@@ -24,7 +44,8 @@ namespace PT2
             if (textBoxTitre.Text == null) { toutPresent = false; }
             else if (nUDPrix.Value == 0) { toutPresent = false; }
             else if (textBoxAlle.Text == null) { toutPresent = false; }
-            else if (pictureBoxPochette.Image == null) { toutPresent = false; }
+            else if (comboBoxEditeur.SelectedItem == null) { toutPresent = false; }
+            else if (comboBoxGenre.SelectedItem == null) { toutPresent = false; }
             return toutPresent;
         }
 
@@ -32,22 +53,39 @@ namespace PT2
         {
             if (VerifPresenceNecessaire())
             {
-                MusiquePT2_DEntities musiqueSQL = new MusiquePT2_DEntities();
                 ALBUMS ajout = new ALBUMS();
-                if (comboBoxEditeur.SelectedItem != null) { ajout.EDITEURS = (EDITEURS)comboBoxEditeur.SelectedItem; }
-                if (comboBoxGenre.SelectedItem != null) { ajout.GENRES = (GENRES)comboBoxGenre.SelectedItem; }
+                if (comboBoxEditeur.SelectedItem != null) 
+                { 
+                    ajout.EDITEURS = (EDITEURS)comboBoxEditeur.SelectedItem;
+                    ajout.CODE_EDITEUR = ajout.EDITEURS.CODE_EDITEUR;
+                }
+                if (comboBoxGenre.SelectedItem != null) 
+                { 
+                    ajout.GENRES = (GENRES)comboBoxGenre.SelectedItem;
+                    ajout.CODE_GENRE = ajout.GENRES.CODE_GENRE;
+                }
                 if (nUDAnneeParution.Value != 0) { ajout.ANNÉE_ALBUM = (int)nUDAnneeParution.Value; }
                 ajout.TITRE_ALBUM = textBoxTitre.Text;
-                ajout.PRIX_ALBUM = (int)nUDPrix.Value;
+                ajout.PRIX_ALBUM = nUDPrix.Value;
                 ajout.ALLÉE_ALBUM = textBoxAlle.Text;
                 ajout.CASIER_ALBUM = (int)nUDCasier.Value;
-                using (var ms = new MemoryStream())
+                ajout.CODE_ALBUM = musiqueSQL.ALBUMS.Count()+1;
+                if (pictureBoxPochette.Image != null)
                 {
-                    pictureBoxPochette.Image.Save(ms, pictureBoxPochette.Image.RawFormat);
-                    ajout.POCHETTE = ms.ToArray();
+                    using (var ms = new MemoryStream())
+                    {
+                        pictureBoxPochette.Image.Save(ms, pictureBoxPochette.Image.RawFormat);
+                        ajout.POCHETTE = ms.ToArray();
+                    }
                 }
                 musiqueSQL.ALBUMS.Add(ajout);
-                musiqueSQL.SaveChanges();
+                /*musiqueSQL.SaveChanges();
+                MessageBox.Show("Importation du vinyle réussi");*/
+                MessageBox.Show("Pour l'instant indisponible");
+            }
+            else
+            {
+                MessageBox.Show("Information manquante");
             }
         }
     }
