6f5202957febb6f3ae50b447d60af2ceae23b203
KrakenKillian <killian.mannarelli@gmail.com>
2021-06-08 10:55:03 +0200

    Création de la classe de test

---
+++ LocationMusique/TestProjetPT2/US5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProjetPT2
+{
+    [TestClass]
+    class US5
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
