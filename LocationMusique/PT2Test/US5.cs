26ca7ccb887373cb909d20cf96730ba1d9f09ce8
KrakenKillian <killian.mannarelli@gmail.com>
2021-06-08 11:37:24 +0200

    US5 test début, push pour les refs

---
+++ LocationMusique/PT2Test/US5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PT2;
+
+namespace PT2Test
+{
+    [TestClass]
+    class US5
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Admin a = new Admin();
+            MusiquePT2_DEntities musiqueSQL = new MusiquePT2_DEntities();
+            List<ABONNÉS> enretardtest = a.enRetard();
+            var Abo1 = from l in musiqueSQL.ABONNÉS select l;
+            Assert.IsTrue(enretardtest.Contains(Abo1.First()));
+
+        }
+    }
+}
