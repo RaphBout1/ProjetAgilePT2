77bf3dd49359ef056c4832f18f6d520374b3411c
KrakenKillian <killian.mannarelli@gmail.com>
2021-06-08 10:25:04 +0200

    Pour boutin

--- LocationMusique/PT2/Connexion.Designer.cs
+++ LocationMusique/PT2/Connexion.Designer.cs
@@ -29,7 +29,6 @@ namespace PT2
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.pseudo = new System.Windows.Forms.TextBox();
             this.mdp = new System.Windows.Forms.TextBox();
             this.connectbutton = new System.Windows.Forms.Button();
@@ -37,16 +36,6 @@ namespace PT2
             this.UtilisateurButton = new System.Windows.Forms.Button();
             this.AdminButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(24, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 37);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Connexion";
             // 
             // pseudo
             // 
@@ -114,7 +103,6 @@ namespace PT2
             this.Controls.Add(this.connectbutton);
             this.Controls.Add(this.mdp);
             this.Controls.Add(this.pseudo);
-            this.Controls.Add(this.label1);
             this.Name = "Connexion";
             this.Text = "Discothèque";
             this.ResumeLayout(false);
@@ -123,8 +111,6 @@ namespace PT2
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox pseudo;
         private System.Windows.Forms.TextBox mdp;
         private System.Windows.Forms.Button connectbutton;
