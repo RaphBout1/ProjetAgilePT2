a6f086ca0b7b888cbce2c5918d557adf6e6247cb
KrakenKillian <killian.mannarelli@gmail.com>
2021-06-09 13:58:42 +0200

    US8test

--- LocationMusique/PT2Test/US8.cs
+++ LocationMusique/PT2Test/US8.cs
@@ -16,6 +16,8 @@ namespace PT2Test
         public ABONNÉS abonné1;
         public ABONNÉS abonné2;
         public Admin user;
+        public UtilisateurUSEmprunt emprunter1;
+        public UtilisateurUSEmprunt emprunter2;
         public ALBUMS album1;
         public ALBUMS album2;
         public EMPRUNTER emprunt1;
@@ -23,19 +25,15 @@ namespace PT2Test
         public EMPRUNTER emprunt3;
         public EMPRUNTER emprunt4;
         public List<EMPRUNTER> listeAlbumsNonEmpruntés;
+        
 
         /*
          * Test de l'US3, si un emprunt n'a jamais été prolongé, on rajoute 1 mois à la date de retour attendue, si oui, ne fait rien.
          */
         public void initTest()
         {
-            abonné1 = (from a in musiqueSQL.ABONNÉS where a.CODE_ABONNÉ == 6 select a).FirstOrDefault();
-            abonné2 = (from a in musiqueSQL.ABONNÉS where a.CODE_ABONNÉ == 7 select a).FirstOrDefault();
-            user = new Admin();
-
-            album1 = (from a in musiqueSQL.ALBUMS where a.CODE_ALBUM == 700 select a).FirstOrDefault();
-            album2 = (from a in musiqueSQL.ALBUMS where a.CODE_ALBUM == 701 select a).FirstOrDefault();
-
+           
+            /*
             emprunt1 = new EMPRUNTER();
             emprunt1.CODE_ABONNÉ = 6;
             emprunt1.ALBUMS = album1;
@@ -43,7 +41,10 @@ namespace PT2Test
             emprunt1.DATE_RETOUR_ATTENDUE = emprunt1.DATE_EMPRUNT.AddDays(album1.GENRES.DÉLAI);
             emprunt1.DATE_RETOUR = DateTime.Parse("9/6/2019 8:30:15 AM");
             musiqueSQL.EMPRUNTER.Add(emprunt1);
-
+            */
+            emprunter1.creerEmprunt(abonné1.CODE_ABONNÉ, album1.CODE_ALBUM, DateTime.Parse("10/6/2019 8:30:15 AM"), DateTime.Parse("11/6/2019 8:30:15 AM"));
+            emprunter2.creerEmprunt(abonné2.CODE_ABONNÉ, album2.CODE_ALBUM, DateTime.Parse("10/6/2019 8:30:15 AM"), DateTime.Parse("11/6/2019 8:30:15 AM"));
+            /*
             emprunt2 = new EMPRUNTER();
             emprunt2.CODE_ABONNÉ = 7;
             emprunt2.ALBUMS = album1;
@@ -68,19 +69,34 @@ namespace PT2Test
             emprunt4.DATE_RETOUR = DateTime.Parse("6/2/2021 8:30:15 AM");
             musiqueSQL.EMPRUNTER.Add(emprunt4);
             musiqueSQL.SaveChanges();
+            */
+            
         }
 
         [TestMethod]
         public void testAlbumsNonEmpruntés()
         {
+            initaliserVariables();
             initTest();
             HashSet<ALBUMS> albumPasEmprunter1An = user.albumPasEmpruntesDepuis1An();
             musiqueSQL.SaveChanges();
-            Assert.IsTrue(albumPasEmprunter1An.Contains((from a in musiqueSQL.ALBUMS where a.CODE_ALBUM == album1.CODE_ALBUM select a).First()));
+            Assert.IsTrue(albumPasEmprunter1An.Contains(album1));
             musiqueSQL.EMPRUNTER.Remove(emprunt1);
             musiqueSQL.EMPRUNTER.Remove(emprunt2);
             musiqueSQL.EMPRUNTER.Remove(emprunt3);
             musiqueSQL.EMPRUNTER.Remove(emprunt4);
+        }
+
+        public void initaliserVariables()
+        {
+            abonné1 = (from a in musiqueSQL.ABONNÉS where a.CODE_ABONNÉ == 6 select a).FirstOrDefault();
+            abonné2 = (from a in musiqueSQL.ABONNÉS where a.CODE_ABONNÉ == 7 select a).FirstOrDefault();
+            user = new Admin();
+            emprunter1 = new UtilisateurUSEmprunt(abonné1);
+            emprunter2 = new UtilisateurUSEmprunt(abonné2);
+
+            album1 = (from a in musiqueSQL.ALBUMS where a.CODE_ALBUM == 700 select a).FirstOrDefault();
+            album2 = (from a in musiqueSQL.ALBUMS where a.CODE_ALBUM == 701 select a).FirstOrDefault();
         }
     }
 }
