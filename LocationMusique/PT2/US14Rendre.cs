06f4abb5c2e225c6856bd88f04ca61b28bbad2bf
KrakenKillian <killian.mannarelli@gmail.com>
2021-06-09 09:02:35 +0200

    Fin US14

--- LocationMusique/PT2/US14Rendre.cs
+++ LocationMusique/PT2/US14Rendre.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 
 namespace PT2
 {
+    /// <summary>
+    /// Une classe Form qui gère le fait de rendre un emprunt.
+    /// </summary>
     public partial class US14Rendre : Form
     {
         ABONNÉS abo;
@@ -20,7 +23,9 @@ namespace PT2
             this.abo = abo;
             remplirEmpruntsBox();
         }
-
+        /// <summary>
+        /// Méthode qui remplit la ListBox de ce form avec tous les emprunts de l'abonné.
+        /// </summary>
         private void remplirEmpruntsBox()
         {
             foreach(EMPRUNTER i in abo.EMPRUNTER)
@@ -33,12 +38,17 @@ namespace PT2
             }
             Refresh();
         }
-
+        /// <summary>
+        /// Un appui sur le bouton rend l'emprunt sélectionné dans la ListBox.
+        /// La date fixée est la date actuelle.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void rendreButton_Click(object sender, EventArgs e)
         {
             EMPRUNTER m = (EMPRUNTER) listBoxEmpruntEnCours.SelectedItem;
             var bonEmprunt = from l in musiqueSQL.EMPRUNTER where l.CODE_ABONNÉ == m.CODE_ABONNÉ && l.CODE_ALBUM == m.CODE_ALBUM select l;
-            bonEmprunt.First().DATE_EMPRUNT = DateTime.UtcNow;
+            bonEmprunt.First().DATE_RETOUR = DateTime.UtcNow;
             musiqueSQL.SaveChanges();
 
         }
